6a0843f9c1cda1af6ee1023fbea873327ff897ea
KaitlynClay <122916928+KaitlynClay@users.noreply.github.com>
2024-02-01 10:59:57 -0600

    Initial Commit

---
+++ ticTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ticTacToe
+//
+//  Created by student on 2/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
